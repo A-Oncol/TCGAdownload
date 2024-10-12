e74f5a9af2b97408c3b5b27a2453b4ae33d365b8
Ao Shen <80802447+A-Oncol@users.noreply.github.com>
2024-10-12 13:04:13 +0800

    Update TCGAdownload.R

--- TCGAdownload.R
+++ TCGAdownload.R
@@ -1,4 +1,3 @@
-#!/usr/bin/Rscript
 ######目的：TCGAbiolinks下载TCGA泛癌多组学数据
 ######作者：申奥
 ######日期：2023-09-14
@@ -17,27 +16,43 @@ spec <- matrix(
   # 每行五个，第五个可选，也就是说第五列可以不写
   # byrow 按行填充矩阵的元素
   # ncol  每行填充五个元素
-  c('help', 'h', 0,'loical', '显示此帮助信息',
-    "cancerType",  "c", 1, "character", "Input a single cancer type"),
-  byrow=TRUE, ncol=5)
+  c("help", "h", 0, "logical", "help information",
+    "cancerType",  "c", 1, "character", "Input the cancer type",
+    "numChunk", "n", 2, "numeric", "Set the files.per.chunk parameter"),
+  byrow = TRUE, ncol = 5)
 opt <- getopt(spec=spec)
+
+if (!is.null(opt$help) || is.null(opt$cancerType) ) {
+  cat(paste(getopt(spec, usage = T), "\n"))
+  q(status=1)
+}
+
+## 设置默认值
+if ( is.null(opt$numChunk) ) {
+  opt$numChunk = 5
+}
 
 
 # 1.设置癌种名称----
 projects <- getGDCprojects()$project_id
-projects
+cat("The GDC projects are as follows:\n", projects, "\n")
 tcga_projects <- projects[grepl("^TCGA", projects, perl = T)]
-tcga_projects
+cat("The TCGA projects are as follows:\n", tcga_projects, "\n")
 cancer_type <- opt$cancerType
+cat("The TCGA projects you choose to download is: ", cancer_type, "\n")
 
 
 # 2.下载并整理表达谱数据----
 ## 2.1 下载----
+cat("Start to download the Gene Expression Quantification for: ", cancer_type, "\n")
 query <- GDCquery(project = cancer_type,
                   data.category = "Transcriptome Profiling",
                   data.type = "Gene Expression Quantification",
                   workflow.type = "STAR - Counts")
 GDCdownload(query = query, directory = "GDCdata", files.per.chunk = 5)
+if (!dir.exists("data/RNA")) {
+  dir.create("data/RNA", recursive = TRUE)
+}
 GDCprepare(query = query, save = T, save.filename = paste0("data/RNA/", cancer_type, "_RNA-seq.RData"))
 
 ## 2.2 提取mRNA和lncRNA的count和TPM数据----
@@ -83,45 +98,66 @@ save(lnc_tpm_annoted, file = paste0("data/RNA/", cancer_type, "_lncRNA_tpm.RData
 
 
 # 3.下载CNV数据----
+cat("Start to download the Copy Number Variation for: ", cancer_type, "\n")
 query <- GDCquery(project = cancer_type,
                   data.category = "Copy Number Variation",
                   data.type = "Masked Copy Number Segment",
                   access = "open")
 GDCdownload(query = query, directory = "GDCdata", files.per.chunk = 2)
+if (!dir.exists("data/CNV")) {
+  dir.create("data/CNV", recursive = TRUE)
+}
 GDCprepare(query = query, save = T, save.filename = paste0("data/CNV/", cancer_type, "_CNV.RData"))
 
 
 # 4.下载突变数据----
+cat("Start to download the Simple Nucleotide Variation for: ", cancer_type, "\n")
 query <- GDCquery(project = cancer_type,
                   data.category = "Simple Nucleotide Variation",
                   data.type = "Masked Somatic Mutation",
                   access = "open")
 GDCdownload(query = query, directory = "GDCdata", files.per.chunk = 5)
+
+if (!dir.exists("data/Mutation")) {
+  dir.create("data/Mutation", recursive = TRUE)
+}
 GDCprepare(query = query, save = T, save.filename = paste0("data/Mutation/", cancer_type, "_Mutation.RData"))
 
 
 # 5.下载miRNA数据----
+cat("Start to download the miRNA Expression Quantification for: ", cancer_type, "\n")
 query <- GDCquery(project = cancer_type,
                   data.category = "Transcriptome Profiling",
                   data.type = "miRNA Expression Quantification")
 GDCdownload(query = query, directory = "GDCdata", files.per.chunk = 2)
+if (!dir.exists("data/RNA")) {
+  dir.create("data/RNA", recursive = TRUE)
+}
 GDCprepare(query = query, save = T, save.filename = paste0("data/RNA/", cancer_type, "_miRNA.RData"))
 
 
 # 6.下载临床数据----
+cat("Start to download the Clinical for: ", cancer_type, "\n")
 clinic_query <- GDCquery(project = cancer_type,
                          data.category = "Clinical", 
                          data.format = "bcr xml")
 GDCdownload(clinic_query)
 clinical <- GDCprepare_clinic(clinic_query, clinical.info = "patient")
+if (!dir.exists("data/Clinical")) {
+  dir.create("data/Clinical", recursive = TRUE)
+}
 save(clinical, file = paste0("data/Clinical/", cancer_type, "_clinical.RData"))
 write.xlsx(clinical, file = paste0("data/Clinical/", cancer_type, "_clinical.xlsx"))
 
 
 # 7.下载甲基化数据----
-query <- GDCquery(project = cancer_type,
-                   data.category = "DNA Methylation",
-                   data.type = "Methylation Beta Value",
-                   platform = "Illumina Human Methylation 450")
-GDCdownload(query = query, directory = "GDCdata", files.per.chunk = 2)
-GDCprepare(query = query, save = T, save.filename = paste0("data/Methylation/", cancer_type, "_Meth.RData"))
+# cat("Start to download the DNA Methylation for: ", cancer_type)
+# query <- GDCquery(project = cancer_type,
+#                    data.category = "DNA Methylation",
+#                    data.type = "Methylation Beta Value",
+#                    platform = "Illumina Human Methylation 450")
+# GDCdownload(query = query, directory = "GDCdata", files.per.chunk = 2)
+# if (!dir.exists("data/Methylation")) {
+#   dir.create("data/Methylation", recursive = TRUE)
+# }
+# GDCprepare(query = query, save = T, save.filename = paste0("data/Methylation/", cancer_type, "_Meth.RData"))
